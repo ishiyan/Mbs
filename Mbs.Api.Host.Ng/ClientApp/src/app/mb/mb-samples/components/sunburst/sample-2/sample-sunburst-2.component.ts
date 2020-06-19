120c3a0596b2c51d790fc88a76a647638b482267
Ivan Shiyan <ishiyan@binck.nl>
2020-06-19 13:01:08 +0200

    sunburst countries

--- Mbs.Api.Host.Ng/ClientApp/src/app/mb/mb-samples/components/sunburst/sample-2/sample-sunburst-2.component.ts
+++ Mbs.Api.Host.Ng/ClientApp/src/app/mb/mb-samples/components/sunburst/sample-2/sample-sunburst-2.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 
-import { SparklineConfiguration } from '../../../../../shared/mbs/charts/sparkline/sparkline-configuration.interface';
+import { HierarchyTreeNode, linearFillOpacity, nameLabels } from '../../../../../shared/mbs/charts/hierarchy-tree';
+import { HierarchyTreeSumFunction, sumNumberOfNodes, sumNumberOfLeafNodes, sumNodeValues } from '../../../../../shared/mbs/charts/hierarchy-tree';
+import { CountryHierarchyTreeNode, countries } from '../../../test-data/hierarchies/countries';
 
+import { SparklineConfiguration } from '../../../../../shared/mbs/charts/sparkline/sparkline-configuration.interface';
 import { Ohlcv } from '../../../../../shared/mbs/data/entities/ohlcv';
 import { Quote } from '../../../../../shared/mbs/data/entities/quote';
 import { Trade } from '../../../../../shared/mbs/data/entities/trade';
 import { Scalar } from '../../../../../shared/mbs/data/entities/scalar';
-
 import { testDataOhlcv } from '../../../test-data/indicators/test-data-ohlcv';
 import { testDataBbBw } from '../../../test-data/indicators/test-data-bb-bw';
 import { testDataBbMa } from '../../../test-data/indicators/test-data-bb-ma';
-
 interface DataItem {
   data: Ohlcv[] | Quote[] | Trade[] | Scalar[];
   name: string;
 }
-
 interface DataGroup {
   disabled?: boolean;
   name: string;
   items: DataItem[];
 }
+///////////////////////////////////////////////////////
+interface SumFunc {
+  value: HierarchyTreeSumFunction;
+  key: string;
+}
+
+const sumFuncArea: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.area ? d.area : 0;
+const sumFuncPopulation: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.population ? d.population : 0;
+const sumFuncDensity: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.density ? d.density : 0;
+const sumFuncHdi: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.hdi ? d.hdi : 0;
+const sumFuncIhdi: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.ihdi ? d.ihdi : 0;
+const sumFuncGdpPerCapita: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.gdpPerCapita ? d.gdpPerCapita : 0;
+const sumFuncIgini: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.igini ? d.igini : 0;
+const sumFuncWgini: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.wgini ? d.wgini : 0;
+const sumFuncSirop: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.sirop ? d.sirop : 0;
+const sumFuncWpaMean: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.wpaMean ? d.wpaMean : 0;
+const sumFuncWpaMedian: HierarchyTreeSumFunction = (d: CountryHierarchyTreeNode) => d.wpaMedian ? d.wpaMedian : 0;
 
 @Component({
   selector: 'mb-sample-sunburst-2',
@@ -29,6 +46,24 @@ interface DataGroup {
 })
 export class SampleSunburst2Component {
 
+  readonly contriesHierarchy: CountryHierarchyTreeNode = countries;
+  readonly sumFuncArray: SumFunc[] = [
+    { key: 'conuntries', value: sumNumberOfLeafNodes },
+    { key: 'area', value: sumFuncArea },
+    { key: 'population', value: sumFuncPopulation },
+    { key: 'population density', value: sumFuncDensity },
+    { key: 'human development index', value: sumFuncHdi },
+    { key: 'inequality-adjusted HDI', value: sumFuncIhdi },
+    { key: 'GDP per capita', value: sumFuncGdpPerCapita },
+    { key: 'income inequality', value: sumFuncIgini },
+    { key: 'Wealth inequality', value: sumFuncWgini },
+    { key: 'share of income top 1%', value: sumFuncSirop },
+    { key: 'mean wealth per adult', value: sumFuncWpaMean },
+    { key: 'median wealth per adult', value: sumFuncWpaMedian },
+  ];
+  sumFuncSelected: HierarchyTreeSumFunction = this.sumFuncArray[0].value;
+
+  //////////////////////////////////////////////////
   dataOhlcv = testDataOhlcv;
   dataScalar = testDataBbBw;
   dataScalarWithNaN = testDataBbMa;

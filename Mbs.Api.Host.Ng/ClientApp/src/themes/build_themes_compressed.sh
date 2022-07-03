f2301a99b7b608d0649830550fe8e4bd3682f6b1
ivan shiyan <ivan.shiyan@gmail.com>
2022-07-03 16:55:45 +0200

    ng update

--- Mbs.Api.Host.Ng/ClientApp/src/themes/build_themes_compressed.sh
+++ Mbs.Api.Host.Ng/ClientApp/src/themes/build_themes_compressed.sh
@@ -1,12 +1,12 @@
 
-sass --quiet --style=compressed --no-source-map indigo-pink.scss:../assets/themes/indigo-pink.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ indigo-pink.scss:../assets/themes/indigo-pink.css
 
-sass --quiet --style=compressed --no-source-map deeppurple-amber.scss:../assets/themes/deeppurple-amber.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ deeppurple-amber.scss:../assets/themes/deeppurple-amber.css
 
-sass --quiet --style=compressed --no-source-map pink-bluegrey.scss:../assets/themes/pink-bluegrey.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ pink-bluegrey.scss:../assets/themes/pink-bluegrey.css
 
-sass --quiet --style=compressed --no-source-map purple-green.scss:../assets/themes/purple-green.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ purple-green.scss:../assets/themes/purple-green.css
 
-sass --quiet --style=compressed --no-source-map yellow-amber.scss:../assets/themes/yellow-amber.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ yellow-amber.scss:../assets/themes/yellow-amber.css
 
-sass --quiet --style=compressed --no-source-map brown-green.scss:../assets/themes/brown-green.css
+sass --quiet --style=compressed --no-source-map --load-path=../../node_modules/ brown-green.scss:../assets/themes/brown-green.css
